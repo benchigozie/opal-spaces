48310b317252f31b0d2f18b312bc6b12d57d91b5
Ben Chigozie <115552805+benchigozie@users.noreply.github.com.>
2025-08-26 11:48:26 +0100

    chore: remove browser router import in app.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import Home from './pages/Home';
 import About from './pages/About';
